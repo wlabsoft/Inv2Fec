2fdb6210c2e2bbef289c3d5b031b30b46fbffcfa
I057743 <wael.labidi@sap.com>
2025-04-16 20:11:08 +0200

    use mistral OCR

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,40 +11,6 @@ model = "mistral-large-latest"
 
 client = Mistral(api_key=os.environ['MISTRAL_API_KEY'])
 
-def get_fec(invoice):
-  prompt = f"""
-            Crée le JSON incluant
-            - Identifiant de la facture (Identifiant)
-            - Date de la facture (Date_Creation)
-            - Date de paiement de la facture (Date_Paiement)
-            - Dénomination du vendeur (Vendeur)
-            - Numéro SIRET du vendeur (SIRET_Vendeur)
-            - Pays du vendeur (Pays_Vendeur)
-            - Dénomination de l'acheteur (Acheteur)
-            - Numéro SIRET de l'acheteur (SIRET_Acheteur)
-            - Pays de l'acheteur (Pays_Acheteur)
-            - Devise de la facture (Devise)
-            - Total HT de la facture (Total_HT)
-            - Total soumis à TVA de la facture (Total_TVA)
-            - Total TTC de la facture (Total_TTC)
-            - Reste à payer (Reste)
-            - Taux de TVA 20%, 10% ou 5.5% (TVA) 
-            - La liste des elements de la facture (Elements), chaque element est défini par ID, Description, Quantite, Prix_HT, TVA et Prix_Totale
-            Pour cette Facture:
-            -------------------
-            {invoice} 
-            -------------------
-            Puis, à partir du JSON de la facture, crée et retourne uniquement sans explication juste le csv (avec ; comme séparateur) 
-            des écritures comptables en format FEC (fichiers des écritures comptables) pour la saisie comptable """
-
-  chat_response = client.chat.complete(
-      model=model,
-      temperature=0.1,
-      messages=[{"role":"user", "content":prompt}]
-  )
-
-  return chat_response.choices[0].message.content
-
 
 def process_files(uploaded_file):
     res = ""
@@ -54,23 +20,60 @@ def process_files(uploaded_file):
         OUTPUT_DIR.mkdir()
         file_path = OUTPUT_DIR / uploaded_file.name
         file_path.write_bytes(uploaded_file.getbuffer())
-        
-        extracted_text = ""
-        # Open the PDF file
-        pdf_document = fitz.open(file_path)
-        
-        for page in pdf_document:
-            # Perform OCR using get_textpage_ocr
-            textpage_ocr = page.get_textpage_ocr(flags=3, dpi=72, full=False, tessdata=None)
 
-            # Extract text from the OCR result
-            extracted_text +=  textpage_ocr.extractText()
-        
-        # Close the PDF document
-        pdf_document.close()
+        uploaded_pdf = client.files.upload(
+            file={
+                "file_name": uploaded_file.name,
+                "content": open(file_path, "rb"),
+            },
+            purpose="ocr"
+        )
 
-        # Convert the OCR result to FEC using LLM
-        res = get_fec(extracted_text)
+        signed_url = client.files.get_signed_url(file_id=uploaded_pdf.id)
+        prompt = f"""
+                    A partir du document (Facture.pdf), crée le JSON incluant
+                    - Identifiant de la facture (Identifiant)
+                    - Date de la facture (Date_Creation)
+                    - Date de paiement de la facture (Date_Paiement)
+                    - Dénomination du vendeur (Vendeur)
+                    - Numéro SIRET du vendeur (SIRET_Vendeur)
+                    - Pays du vendeur (Pays_Vendeur)
+                    - Dénomination de l'acheteur (Acheteur)
+                    - Numéro SIRET de l'acheteur (SIRET_Acheteur)
+                    - Pays de l'acheteur (Pays_Acheteur)
+                    - Devise de la facture (Devise)
+                    - Total HT de la facture (Total_HT)
+                    - Total soumis à TVA de la facture (Total_TVA)
+                    - Total TTC de la facture (Total_TTC)
+                    - Reste à payer (Reste)
+                    - Taux de TVA 20%, 10% ou 5.5% (TVA) 
+                    - La liste des elements de la facture (Elements), chaque element est défini par ID, Description, Quantite, Prix_HT, TVA et Prix_Totale
+                    Puis, à partir du JSON de la facture, crée et retourne uniquement sans explication juste le csv (avec ; comme séparateur) 
+                    des écritures comptables en format FEC (fichiers des écritures comptables) pour la saisie comptable """
+
+        messages = [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": prompt
+                    },
+                    {
+                        "type": "document_url",
+                        "document_url": signed_url.url
+                    }
+                ]
+            }
+        ]
+
+        # Get the chat response
+        chat_response = client.chat.complete(
+            model="mistral-large-latest",
+            messages=messages
+        )
+
+        res = chat_response.choices[0].message.content
 
     except Exception as e:
         st.error(f"Erreur avec {uploaded_file.name}: {str(e)}")
